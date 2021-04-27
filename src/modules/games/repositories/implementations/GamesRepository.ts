3d7b9a56770f258eab2157b0d65784edf145c804
Bruno Santos <brcesar19@gmail.com>
2021-04-27 19:21:25 -0300

    solving the test

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -14,17 +14,22 @@ export class GamesRepository implements IGamesRepository {
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder()
+      .createQueryBuilder("games")
+      .where('games.title ILIKE %:val%', {val: param})
+      .getMany()
       // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT count(`id`) FROM `games`'); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
       .createQueryBuilder()
+      .relation(Game, 'users')
+      .of(id)
+      .loadMany()
       // Complete usando query builder
   }
 }
